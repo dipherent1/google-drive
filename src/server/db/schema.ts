ca5de0b3f85bae6b580e3b580d50d0431f5ee6da
dipherent1 <binyammulat244@gmail.com>
2025-12-31 22:18:39 +0300

    refactor: restructure schema to use createTable for file and folder definitions; remove unused user table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,8 +1,34 @@
-import { int, text, singlestoreTable } from "drizzle-orm/singlestore-core";
+import {
+  bigint,
+  index,
+  text,
+  float,
+  singlestoreTableCreator,
+} from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("users_table", {
-  id: int("id").primaryKey().autoincrement(),
+export const createTable = singlestoreTableCreator(
+  (name) => `google-drive_${name}`,
+);
+
+export const files = createTable(
+  "file_table",
+  {
+    id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
+    name: text("name").notNull(),
+    url: text("url").notNull(),
+    size: float("size").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent!)];
+  },
+);
+
+export const folder = createTable(
+  "folder_table", 
+  {
+  id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
   name: text("name").notNull(),
-  email: text("email").notNull().unique(),
-  passwordHash: text("password_hash").notNull(),
+  modified: text("modified").notNull(),
+  parent: bigint("parent", { mode: "number", unsigned: true }),
 });
